6763eea44cd23335de43526fa381a40e12e80e46
Juan <juancardenas083@gmail.com>
2026-05-07 21:12:37 -0500

    Add modals login funcion

--- src/components/comHome/Nav.jsx
+++ src/components/comHome/Nav.jsx
@@ -3,9 +3,23 @@ import { Button, Container, Navbar } from "react-bootstrap";
 import { LuShoppingBasket } from "react-icons/lu";
 import { Outlet } from "react-router-dom";
 import Modal_Login from "../comLogin/Modal-Login";
+import Modal_Register from "../comLogin/Modal-Register";
 
 export default function NavBar() {
     const [show, setShow] = useState(false);
+    const [showregister, setShowregister] = useState(false); //Variables para mostrar el modal de registro
+
+    const MostrarModals = (modal) => {
+        if (show === true) {
+            setShow(false);
+            setShowregister(true);
+        } else if (showregister === true) {
+            setShow(false);
+            setShowregister(false);
+        } else {
+            setShow(true);
+        }
+    }
 
     return (
         <>
@@ -15,10 +29,11 @@ export default function NavBar() {
                     <h5>Shop proyect</h5>
                     <p className="fs-text-nav m-0 p-0 d-flex align-items-center"><LuShoppingBasket />Tienda Online</p>
                 </Navbar.Brand>
-                <Button className="fw-semibold app-color-button" onClick={() => setShow(true)}>Iniciar Sesion</Button>
+                <Button className="fw-semibold app-color-button" onClick={MostrarModals}>Iniciar Sesion</Button>
             </Container>
         </Navbar>
-        <Modal_Login show={show} ocultar={() => setShow(false)} />
+        <Modal_Login show={show} ocultar={MostrarModals} />
+        <Modal_Register show={showregister} ocultar={MostrarModals} />
         {/*Oulet sirve para definir donde se renderiza el contenido de las paginas despues del NAV*/}
         <Outlet />
         </>
--- src/components/comLogin/Modal-Register.jsx
+++ src/components/comLogin/Modal-Register.jsx
@@ -1,15 +1,13 @@
 import { Button, Form, Modal } from "react-bootstrap";
-import './style-modals-login.css';
-import { useState } from "react";
 
-export default function Modal_Login( { show, ocultar}) {
+export default function Modal_Register( { show, ocultar } ) {
     return (
         <>
         <Modal show={show} onHide={ocultar} centered>
             <Modal.Header closeButton>
                 <Modal.Title className="text-center w-100">
                     <p className="p-0 m-0">E-Commerce</p>
-                    <p className="fw-normal p-0 m-0 fs-6 text-muted">Inicia sesion en tu cuenta</p>
+                    <p className="fw-normal p-0 m-0 fs-6 text-muted">Registro modal 2</p>
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
@@ -25,10 +23,11 @@ export default function Modal_Login( { show, ocultar}) {
                     <Button className="mb-4 w-100 app-color-button fw-semibold">Iniciar sesion</Button>
                 </Form>
                 <div className="text-center">
-                    <a onClick={ocultar} href="#" className="fw-semibold link-create-acc">¿No tienes cuenta?</a>
+                    <a href="#" className="fw-semibold link-create-acc">¿No tienes cuenta?</a>
                 </div>
             </Modal.Body>
         </Modal>
         </>
     )
+    
 }
